eb0f184dda9e6f8305a4868112f5550dd0096ed3
bjmashele <bjmashele@gmail.com>
2026-06-20 12:51:21 -0500

    Add and configure Logging module

--- server.py
+++ server.py
@@ -5,10 +5,18 @@ from collections import Counter
 import requests
 from bs4 import BeautifulSoup
 from mcp.server.fastmcp import FastMCP
+import logging
+
+
+logging.basicConfig(
+     format="{asctime} - {levelname} - {message}",
+     style="{",
+     datefmt="%Y-%m-%d %H:%M",
+ )
 
 # Create an MCP server
 mcp = FastMCP(
-    "demo",
+    "competitor_analysis",
     host="127.0.0.1",
     port=8000
 )
